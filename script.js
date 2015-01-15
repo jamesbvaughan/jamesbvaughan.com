a0a5f717895bb3818a96c61ac47190fd760b2de9
James Vaughan <james@jamesbvaughan.com>
2015-01-15 12:45:19 -0800

    actually fixed the now playing issue

--- script.js
+++ script.js
@@ -18,17 +18,23 @@ $(document).ready(function () {
     var url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1&user=magicjamesv&api_key=9csd784e60b827a3983491b3e91baf82&format=json';
     
     $.getJSON((url.substring(0, 96) + x.substring(11, 42) + url.substring(127)), function (data) {
-        if (data.recenttracks.track[0]['@attr'].nowplaying === "true") {
+        try {
             document.getElementById("musicTitle").innerHTML = "James is listening to:";
-        } else {
+            document.getElementById("song").innerHTML = data.recenttracks.track[0].name;
+            document.getElementById("artist").innerHTML = "<i>" +
+                data.recenttracks.track[0].artist['#text'] + "</i>";
+            document.getElementById("albumArt").innerHTML = "<img src='" +
+                data.recenttracks.track[0].image[1]['#text'] +
+                "' alt='album art' height='64' width='64'></img>";
+        } catch (err) {
             document.getElementById("musicTitle").innerHTML = "James last listened to:";
+            document.getElementById("song").innerHTML = data.recenttracks.track.name;
+            document.getElementById("artist").innerHTML = "<i>" +
+                data.recenttracks.track.artist['#text'] + "</i>";
+            document.getElementById("albumArt").innerHTML = "<img src='" +
+                data.recenttracks.track.image[1]['#text'] +
+                "' alt='album art' height='64' width='64'></img>";
         }
-        document.getElementById("song").innerHTML = data.recenttracks.track[0].name;
-        document.getElementById("artist").innerHTML = "<i>" +
-            data.recenttracks.track[0].artist['#text'] + "</i>";
-        document.getElementById("albumArt").innerHTML = "<img src='" +
-            data.recenttracks.track[0].image[1]['#text'] +
-            "' alt='album art' height='64' width='64'></img>";
     });
     
 });
