0350ad0f1ad42ab9ae02f4cead060a7a92ac47e6
James Vaughan <james@jamesbvaughan.com>
2015-01-19 17:04:48 -0800

    major site redesign

--- js/script.js
+++ js/script.js
@@ -3,21 +3,7 @@
 $(document).ready(function () {
     "use strict";
 
-    // This controls the movements of the links
-    $("a").hover(function () {
-        $(this).stop().animate({
-            marginLeft: "5px"
-        }, "fast");
-    }, function () {
-        $(this).stop().animate({
-            marginLeft: "0px"
-        }, "fast");
-    });
-    
-    var x = '2je33u4bd89cec0534e60b827aab0ae1b3e91baf8228fg0rk487z0sn2z9sn3484b560ng98';
-    var url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1&user=magicjamesv&api_key=9csd784e60b827a3983491b3e91baf82&format=json';
-    
-    $.getJSON((url.substring(0, 96) + x.substring(11, 42) + url.substring(127)), function (data) {
+    $.getJSON('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1&user=magicjamesv&api_key=9cec0534e60b827aab0ae1b3e91baf82&format=json', function (data) {
         try {
             document.getElementById("musicTitle").innerHTML = "James is listening to:";
             document.getElementById("song").innerHTML = data.recenttracks.track[0].name;
@@ -36,5 +22,5 @@ $(document).ready(function () {
                 "' alt='album art' height='64' width='64'></img>";
         }
     });
-    
+
 });
