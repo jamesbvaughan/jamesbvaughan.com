19ce9ecd2689c846ec5b9c2e8fe9886277da55a9
James Vaughan <james@jamesbvaughan.com>
2015-01-19 22:54:02 -0800

    added now playing stuff

--- js/script.js
+++ js/script.js
@@ -5,21 +5,9 @@ $(document).ready(function () {
 
     $.getJSON('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1&user=magicjamesv&api_key=9cec0534e60b827aab0ae1b3e91baf82&format=json', function (data) {
         try {
-            document.getElementById("musicTitle").innerHTML = "James is listening to:";
-            document.getElementById("song").innerHTML = data.recenttracks.track[0].name;
-            document.getElementById("artist").innerHTML = "<i>" +
-                data.recenttracks.track[0].artist['#text'] + "</i>";
-            document.getElementById("albumArt").innerHTML = "<img src='" +
-                data.recenttracks.track[0].image[1]['#text'] +
-                "' alt='album art' height='64' width='64'></img>";
+            $("#nowPlaying").html("Right now I'm listening to " + data.recenttracks.track[0].name + " by " + data.recenttracks.track[0].artist['#text'] + ".");
         } catch (err) {
-            document.getElementById("musicTitle").innerHTML = "James last listened to:";
-            document.getElementById("song").innerHTML = data.recenttracks.track.name;
-            document.getElementById("artist").innerHTML = "<i>" +
-                data.recenttracks.track.artist['#text'] + "</i>";
-            document.getElementById("albumArt").innerHTML = "<img src='" +
-                data.recenttracks.track.image[1]['#text'] +
-                "' alt='album art' height='64' width='64'></img>";
+            $("#nowPlaying").html("Right now I'm listening to " + data.recenttracks.track.name + " by " + data.recenttracks.track[0].artist['#text'] + ".");
         }
     });
 
