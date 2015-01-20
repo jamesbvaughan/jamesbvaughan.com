ff1d5124d3e55cd0be46a6ee130e43f75ac9ddbb
James Vaughan <james@jamesbvaughan.com>
2015-01-19 23:42:54 -0800

    changed placement of now playing info

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ $(document).ready(function () {
 
     $.getJSON('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1&user=magicjamesv&api_key=9cec0534e60b827aab0ae1b3e91baf82&format=json', function (data) {
         try {
-            $("#nowPlaying").html("Right now I'm listening to " + data.recenttracks.track[0].name + " by " + data.recenttracks.track[0].artist['#text'] + ".");
+            $("#nowPlaying").append(" (Right now I'm listening to <a href='" + data.recenttracks.track[0].url + "'>" + data.recenttracks.track[0].name + " by " + data.recenttracks.track[0].artist['#text'] + "</a>.)");
         } catch (err) {
-            $("#nowPlaying").html("Right now I'm listening to " + data.recenttracks.track.name + " by " + data.recenttracks.track[0].artist['#text'] + ".");
+            $("#nowPlaying").append("(I last listened to <a href='" + data.recenttracks.track.url + "'>" + data.recenttracks.track.name + " by " + data.recenttracks.track.artist['#text'] + ".)");
         }
     });
 
