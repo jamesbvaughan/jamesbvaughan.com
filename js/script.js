965fd2934fbf1ca9713edc48446b3f077c2d745a
James Vaughan <james@jamesbvaughan.com>
2016-02-21 03:39:17 -0800

    updated look

--- js/script.js
+++ js/script.js
@@ -3,13 +3,11 @@ $(document).ready(function () {
 
 	var url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1&user=magicjamesv&api_key=9cec0534e60b827aab0ae1b3e91baf82&format=json';
 
-	var ret = $.getJSON(url, function (data) {
-		var text;
-		try {
-			text = " (Right now I'm listening to <a href='" + data.recenttracks.track[0].url + "'>" + data.recenttracks.track[0].name + " by " + data.recenttracks.track[0].artist['#text'] + "</a>.)";
-		} catch (err) {
-			text = " (I last listened to <a href='" + data.recenttracks.track.url + "'>" + data.recenttracks.track.name + " by " + data.recenttracks.track.artist['#text'] + "</a>.)";
-		}
-		$("#nowPlaying").append(text);
+	$.get(url, function (data) {
+		var track = data.recenttracks.track;
+		var isArray = track.constructor === Array;
+		track = isArray ? track[0] : track;
+		$(".nowPlaying").append(isArray ? "At the moment I'm listening to" : "The last song I listened to was");
+		$(".nowPlaying").append(" <a href='" + track.url + "'>" + track.name + " by " + track.artist['#text'] + "</a>.)");
 	});
 });
