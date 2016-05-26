baa62c05c79eb1db4c0cf6378994fb40b72c9652
James Vaughan <james@jamesbvaughan.com>
2016-05-25 20:36:48 -0700

    cleaned up code

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 const url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1&user=magicjamesv&api_key=9cec0534e60b827aab0ae1b3e91baf82&format=json'
 
-fetch(url).then(data => data.json()).then(data =>
-  document.getElementById("nowPlaying").innerHTML = `
-    ${data.recenttracks.track.length > 1 ? "(At the moment I'm listening to "
-      : "(The last song I listened to was "}
-    <a href='${data.recenttracks.track[0].url}'>
-      ${data.recenttracks.track[0].name}
-      by ${data.recenttracks.track[0].artist['#text']}
-    </a>.)`)
+fetch(url)
+  .then(data => data.json())
+  .then(json => json.recenttracks.track)
+  .then(tracks =>
+    document.getElementById("nowPlaying").innerHTML = `
+      (${tracks.length > 1 ? "At the moment I'm listening to "
+        : "The last song I listened to was "}
+      <a href='${tracks[0].url}'>
+        ${tracks[0].name} by ${tracks[0].artist['#text']}
+      </a>.)`)
 
 document.getElementById("projects").innerHTML +=
-  projects.map(project => `
+  projects.map(({url, name, tools, description, year}) => `
 		<div class="project">
       <div class="title">
-        <h3><a href=${project.url} target="_blank">${project.name}</a></h3>
+        <h3><a href=${url} target="_blank">${name}</a></h3>
         <div class="toolList">
-          ${project.tools.map(tool => '<span>'+tool+'</span>').join('')}
+          ${tools.map(tool => '<span>'+tool+'</span>').join('')}
         </div>
       </div>
-      <div>${project.description} (${project.year})</div>
+      <div>${description} (${year})</div>
 		</div>`).join('')
