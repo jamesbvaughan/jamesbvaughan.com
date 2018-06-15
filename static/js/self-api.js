dcb2f62e02d8545b647715dd19c4ea84401523de
James Vaughan <james@jamesbvaughan.com>
2018-06-15 11:13:38 -0700

    using npm

--- static/js/self-api.js
+++
@@ -1,6 +0,0 @@
-['song', 'movie'].forEach(item =>
-  fetch(`https://api.jamesbvaughan.com/${item}`)
-    .then(r => r.json())
-    .then(({ status, body }) =>
-      status === 'ok' &&
-        (document.getElementById(item).innerHTML = ` (${body})`)))
