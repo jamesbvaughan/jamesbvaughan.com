ec0e1dbeecef5b2c482a74468681761699034102
James Vaughan <james@jamesbvaughan.com>
2019-11-09 14:17:19 -0800

    Fix albums.js

--- functions/src/albums.js
+++ functions/src/albums.js
@@ -1,9 +1,11 @@
 import fetch from 'node-fetch'
 
-const discogsURL = `https://api.discogs.com/lists/447073?token=${process.env.DISCOGS_TOKEN}`
+const discogsURL =
+  `https://api.discogs.com/lists/447073?token=${process.env.DISCOGS_TOKEN}`
 
-exports.handler = (_, _, callback) =>
+exports.handler = (_event, _context, callback) =>
   fetch(discogsURL)
     .then(res => res.json())
-    .then(({ items }) => callback(null, { statusCode: 200, body: JSON.stringify(items) }))
+    .then(({ items }) =>
+      callback(null, { statusCode: 200, body: JSON.stringify(items) }))
     .catch(err => callback(err))
